c0cddafa813d9e5ff245ab38323c09b17da383b3
gong zhaopeng <gongzhaopeng@gongs-MacBook-Air.local>
2019-01-17 19:03:03 +0800

    ...

---
+++ src/main/java/com/atzu68/spia5/configrepo/tacoloud/TacoCloudConfigRepoApplication.java
@@ -0,0 +1,14 @@
+package com.atzu68.spia5.configrepo.tacoloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudConfigRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudConfigRepoApplication.class, args);
+    }
+
+}
+
